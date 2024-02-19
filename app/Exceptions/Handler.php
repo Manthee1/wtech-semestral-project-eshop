e814bd47c295d1c2af4af22117da48f940c5059c
Martin Dinja <martin@webgaleria.sk>
2024-02-19 20:17:24 +0100

    Added authentication stackholding

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,37 +7,43 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-	/**
-	 * The list of the inputs that are never flashed to the session on validation exceptions.
-	 *
-	 * @var array<int, string>
-	 */
-	protected $dontFlash = [
-		'current_password',
-		'password',
-		'password_confirmation',
-	];
+    /**
+     * The list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
 
-	/**
-	 * Register the exception handling callbacks for the application.
-	 */
-	public function register(): void
-	{
-		$this->reportable(function (Throwable $e) {
-		});
-	}
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+        });
+    }
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param \Illuminate\Http\Request $request
-	 * @param Throwable $e
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 *
-	 * @throws Throwable
-	 */
-	public function render($request, Throwable $e)
-	{
-		return parent::render($request, $e);
-	}
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Throwable $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws Throwable
+     */
+    public function render($request, Throwable $e)
+    {
+
+        // If we are visiting an auth route but we are not logged in, redirect to the login page
+        if ($e instanceof \Illuminate\Auth\AuthenticationException) {
+            return redirect()->route('login');
+        }
+
+        return parent::render($request, $e);
+    }
 }
