a113631059747ef48f8c66fb46f72e004f6a421e
Manthee1 <dinja.martin@gmail.com>
2024-05-08 17:23:00 +0200

    Removed unused libraries Added card verification, laravel ui and laravel html libraries

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -48,6 +48,17 @@
       0 => 'Termwind\\Laravel\\TermwindServiceProvider',
     ),
   ),
+  'spatie/laravel-html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Html' => 'Spatie\\Html\\Facades\\Html',
+    ),
+  ),
   'spatie/laravel-ignition' => 
   array (
     'providers' => 
