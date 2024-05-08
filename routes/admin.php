9de4d2d562cfcbac6d93cb1a5fdb49961ce67d71
Manthee1 <dinja.martin@gmail.com>
2024-05-08 17:14:19 +0200

    Added al ladmin routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,20 +2,33 @@
 
 // Laravel admin routes
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
-Auth::routes();
-
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+use App\Http\Controllers\ProductController;
+use App\Models\Order;
 
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/admin', function () {
-        return view('web.backend.dashboard');
+
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('/', function () {
+
+        $latestOrders = Order::latest()->take(10)->get();
+        $totalOrders = Order::count();
+        $earnings = [
+            'lastYear' => Order::whereYear('created_at', now()->year)->sum('total'),
+            'lastMonth' => Order::whereMonth('created_at', now()->month)->sum('total'),
+            'lastWeek' => Order::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->sum('total'),
+            'lastDay' =>  Order::whereDate('created_at', now()->today())->sum('total'),
+        ];
+
+        return view('backend.dashboard', [
+            'latestOrders' => $latestOrders,
+            'totalOrders' => $totalOrders,
+            'earnings' => $earnings,
+        ]);
     })->name('admin.dashboard');
+
+    Route::resource('products', ProductController::class);
+
+    Route::get('/attributes', function () {
+        return view('backend.attributes');
+    })->name('admin.attributes');
 });
