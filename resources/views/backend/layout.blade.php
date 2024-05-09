7bcc9152a0cdf3d59c9701784c7121964806935e
Manthee1 <dinja.martin@gmail.com>
2024-05-09 16:45:49 +0200

    Updated the backend layout meta tags

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -11,11 +11,16 @@
     <script nomodule="" src="https://unpkg.com/ionicons@5.1.2/dist/ionicons/ionicons.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Martin Dinja">
 
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
 
-    @vite(['resources/sass/main.sass', 'resources/js/main.js'])
+    @vite(['resources/sass/main.sass', 'resources/js/main.js'], 'build')
 
     @yield('styles')
 </head>
